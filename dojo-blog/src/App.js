e683507d2c354c5eb9538b1319bf63dc456f18cb
Sean Mapanao <sean.mapanao@gmail.com>
2022-08-24 19:49:06 -0400

    Created nav bar component and created the template of it

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
+//component is a function and we must always return something in that function. Written in JSX template
 function App() {
+
+  //any valid javascript can be written here
+
+  const title = "App Component";
+  
+  //react automatically converts to string when used below. Numbers, strings and arrays are fine by react
+  //const likes = 50; 
+
+  //will not be converted. Objects and booleans cannot be converted by react
+  //const person = {name: "Yoshi", age: 30};
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+       
+      <h1>{ title } </h1>
+      
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; //always export component function so we can use it in other files. We can import it in other files by adding this
